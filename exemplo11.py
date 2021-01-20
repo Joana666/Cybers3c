e3bf3f16e826db356d6ef5a09715a9d5bec6950b
Joana666 <77698198+Joana666@users.noreply.github.com>
2021-01-20 18:37:34 +0000

    Update exemplo11.py

--- exemplo11.py
+++ exemplo11.py
@@ -1,1 +1,36 @@
 
+#!/usr/bin/python
+a=1
+b=2
+#print (a+b)
+#print (a<b)
+#print (a*b)
+
+
+
+#c=[1,2,3,4,5]
+#d=3 in c
+#print(d)
+
+#print (bin(a|b))
+
+
+
+
+
+#contador =0
+#while ( contador < 5 ):
+ #print (contador)
+ #contador= contador + 1
+#else: 
+ #print ("Loop terminou")
+
+x = ['windows','macos','linux','solaris','android','ios']
+  #while x =! "solaris"
+   #else x +1
+for item in x 
+    if then!='solaris'
+print (item) 
+ 
+
+print (x)
